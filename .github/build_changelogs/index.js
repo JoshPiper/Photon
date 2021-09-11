21279d6f97aefc88230cd78c767fb309d41093e4
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-09-11 23:30:38 +0100

    convert the stream to a string.

--- .github/build_changelogs/index.js
+++ .github/build_changelogs/index.js
@@ -3,19 +3,40 @@ const github = require('@actions/github')
 const {inspect} = require("util")
 const changelog = require("conventional-changelog-core")
 
+function stream_to_string(stream, encoding="utf8"){
+	/** @type {Buffer[]} buffers */
+	let buffers = []
+
+	return new Promise((resolve, reject) => {
+		stream.on("data", data => {
+			if (data instanceof Buffer){
+				buffers.push(data)
+			} else if (typeof data === "string"){
+				buffers.push(Buffer.from(data))
+			} else {
+				reject("Only non-object mode buffers are accepted.")
+			}
+		})
+
+		stream.on("error", err => reject(err))
+		stream.on("end", () => resolve(Buffer.concat(buffers).toString(encoding)))
+	})
+}
+
 async function run(){
 	const token = core.getInput("token")
 	const before = core.getInput("before")
 	const after = core.getInput("after")
 
-	let buffer = changelog({
+	let stream = changelog({
 		context: {
 			previousTag: before,
 			currentTag: after
 		}
 	})
-	core.info(buffer)
-	core.info(typeof buffer)
-	core.info(inspect(buffer))
+	core.info(stream)
+
+	let changelog = stream_to_string(stream)
+	core.info(changelog)
 }
 run()
