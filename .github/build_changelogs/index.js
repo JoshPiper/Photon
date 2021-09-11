fbe30c38ec1ad91b9c2dfef0b19ed0e4b56415dc
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-09-11 22:59:11 +0100

    push up the changelog builder.

---
+++ .github/build_changelogs/index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const {inspect} = require("util")
+const changelog = require("conventional-changelog-core")
+
+async function run(){
+	const token = core.getInput("token")
+	const before = core.getInput("before")
+	const after = core.getInput("after")
+
+	let buffer = changelog({
+		context: {
+			previousTag: before,
+			currentTag: after
+		}
+	})
+	core.info(buffer)
+	core.info(typeof buffer)
+}
