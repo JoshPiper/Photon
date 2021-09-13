b3496a02c87f8e6429a4355ebfaf4a2af0602d42
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-09-13 22:39:32 +0100

    feat: automatically generate the new series title and output it.

--- .github/photon_title/index.js
+++ .github/photon_title/index.js
@@ -1,6 +1,16 @@
 const core = require('@actions/core')
 const SemVer = require("semver-parser")
 
+let used = require("./used.json")
+let municipalities = require("./data.json")
+
+const letters = [
+	"a", "b", "c", "d", "e", "f", "g",
+	"h", "i", "j", "k", "l", "m", "n",
+	"o", "p", "q", "r", "s", "t", "u",
+	"v", "w", "x", "y", "z"
+]
+
 /**
  * Appends any required ".0" to make a partial tag into a full semver tag (v75 -> v75.0.0)
  * @param {string} tag
@@ -18,26 +28,87 @@ function make_full(tag){
 	return null
 }
 
+function pack(municipalities){
+	let starter = {}
+
+	for (let municipality of municipalities){
+		let letter = municipality.substr(0, 1).toLowerCase()
+		starter[letter] = (starter[letter] !== undefined ? starter[letter] : [])
+		starter[letter].push(municipality)
+	}
+
+	return starter
+}
+
 async function run(){
 	const token = core.getInput("token")
 	const tag = make_full(core.getInput("next"))
 	const version = SemVer.parseSemVer(tag)
 
 	if (version.minor !== 0 && version.minor !== undefined){
-		core.setOutput("series", "")
+		core.setOutput("series", used[version.major.toString()])
 		core.warning("Patch versions don't get an updated series title.")
 		return
 	}
-	/** @type {string[]} */
-	let data = require("./data.json")
-	let starter = {}
-	for (let municipality of data){
-		let letter = municipality.substr(0, 1).toLowerCase()
-		starter[letter] = (starter[letter] !== undefined ? starter[letter] : 0) + 1
+
+	/** @type {{string: string[]}} */
+	let starter = pack(municipalities)
+
+	let availableLetters = Object.keys(starter)
+	availableLetters = availableLetters.sort((a, b) => a.localeCompare(b))
+	let count = availableLetters.length
+
+	core.info(`There are ${count} letters with active municipalities, totalling ${municipalities.length}.`)
+	core.info(`Current version is: ${version.major}`)
+	core.info(`v71 was cycle 1, 'e', replaying from there.`)
+
+	let letterId = 4
+	for (let major = 71; major <= version.major; major++){
+		core.startGroup(`v${major}`)
+		if (used[major] !== undefined){
+			let usedCity = used[major.toString()]
+			let letter = usedCity.substr(0, 1).toLowerCase()
+			letterId = letters.indexOf(letter)
+			core.info(`v${major} used ${usedCity}, removing from data.`)
+			municipalities = municipalities.filter(municipality => municipality !== usedCity)
+		} else {
+			if (major !== version.major){
+				core.warning(`v${major} was not saved!`)
+			}
+
+			if (municipalities.length === 0){
+				core.setFailed("We have ran out of municipalities!")
+				return
+			}
+
+			core.info("Repacking Municipalities")
+			starter = pack(municipalities)
+
+			letterId = (letterId + 1) % letters.length
+			let letter = letters[letterId]
+			core.info(`This version's name should be start with ${letter}`)
+
+			while (starter[letter] === undefined){
+				core.info(`${letter} has no available municipalities`)
+				letterId = letterId = (letterId + 1) % letters.length
+				letter = letters[letterId]
+			}
+
+			core.info(`This version will start with ${letter}`)
+			let cities = starter[letter]
+			core.info(`There are ${cities.length} possible options: ${cities.join(", ")}`)
+			let pick = cities[Math.floor(Math.random() * cities.length)]
+			core.info(`We will use ${pick}.`)
+
+			used[major.toString()] = pick
+
+			if (major === version.major){
+				core.setOutput("series", pick)
+			}
+		}
+		core.endGroup()
 	}
 
-	let count = Object.keys(starter).length
-	core.info(`There are ${count} letters with active municipalities`)
-
+	require("fs").writeFileSync(require("path").resolve(__dirname, "used.json"), JSON.stringify(used))
 }
 run()
